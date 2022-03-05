d9b7cb9650e849540bf4cf50a51b4b35be04d074
aarts <aartschouten@hotmail.com>
2022-03-05 19:38:09 +0100

    Feedback domain and Feedbacktests

---
+++ src/main/java/nl/hu/cisq1/lingo/trainer/domain/Feedback.java
@@ -0,0 +1,65 @@
+package nl.hu.cisq1.lingo.trainer.domain;
+
+import org.hibernate.validator.internal.constraintvalidators.hv.ru.INNValidator;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+
+public class Feedback {
+    private String attempt;
+    private String wordToGuess;
+    private List<Mark> marks;
+
+    public Feedback(String attempt, List<Mark> marks) {
+        this.attempt = attempt;
+        this.marks = marks;
+    }
+
+    public static Feedback invalid(String attempt, String wordToGuess) {
+        List<Mark> marks = Collections.nCopies(wordToGuess.length(), Mark.INVALID);
+        return new Feedback(attempt, marks);
+    }
+
+    public boolean isWordGuessed() {
+        return marks.stream()
+                .allMatch(Mark.CORRECT::equals);
+    }
+
+    public boolean isAttemptInvalid() {
+        return marks
+                .stream()
+                .anyMatch(Mark.INVALID::equals);
+    }
+
+    public String giveHint(String previousHint, String wordToGuess) {
+        StringBuilder result = new StringBuilder();
+
+        // G R O E P
+        // G E N E N
+        // G . . E .
+
+        for (int i = 0; i < marks.size(); i++) {
+            if (marks.get(i).equals(Mark.CORRECT)) {
+                result.append(wordToGuess.charAt(i));
+            } else {
+                result.append(previousHint.charAt(i));
+            }
+        }
+        return result.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Feedback feedback = (Feedback) o;
+        return Objects.equals(attempt, feedback.attempt) && Objects.equals(marks, feedback.marks);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(attempt, marks);
+    }
+}
---
+++ src/test/java/nl/hu/cisq1/lingo/trainer/domain/FeedbackTest.java
@@ -0,0 +1,71 @@
+package nl.hu.cisq1.lingo.trainer.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FeedbackTest {
+    @Test
+    @DisplayName("word is guessed if all letters are correct")
+    void wordIsGuessed() {
+        String attempt = "groep";
+        String wordToGuess = "groep";
+        List<Mark> marks = List.of(Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT);
+
+        Feedback feedback = new Feedback(attempt, marks );
+
+        assertTrue(feedback.isWordGuessed());
+    }
+
+    @Test
+    @DisplayName("attempt is invalid if a letter is marked invalid")
+    void attemptIsInvalid() {
+        String attempt = "groepen";
+        String wordToGuess = "groep";
+
+        Feedback feedback = Feedback.invalid(attempt, wordToGuess);
+
+        assertTrue(feedback.isAttemptInvalid());
+    }
+
+    @ParameterizedTest
+    @MethodSource("hintExamples")
+    @DisplayName("give hint based on previous hint and word to guess")
+    void giveHint(String previousHint, String wordToGuess, String attempt, List<Mark> marks, String exptectedHint) {
+        Feedback feedback = new Feedback(attempt, marks);
+
+        String nextHint = feedback.giveHint(previousHint, wordToGuess);
+
+        assertEquals(exptectedHint, nextHint);
+
+    }
+
+    static Stream<Arguments> hintExamples() {
+        return Stream.of(
+                Arguments.of("g....", "groep", "genen", List.of(Mark.CORRECT, Mark.ABSENT, Mark.ABSENT, Mark.CORRECT, Mark.ABSENT), "g..e."),
+                Arguments.of("g..e.", "groep", "gedoe", List.of(Mark.CORRECT, Mark.PRESENT, Mark.ABSENT, Mark.PRESENT, Mark.ABSENT), "g..e.")
+        );
+    }
+
+
+    @Test
+    @DisplayName("feedback is equal to other feedback")
+    void equals() {
+        String attempt = "woord";
+
+        List<Mark> marks1 = List.of(Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT);
+        List<Mark> marks2 = List.of(Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT);
+
+        Feedback feedback1  = new Feedback(attempt, marks1 );
+        Feedback feedback2  = new Feedback(attempt, marks2 );
+
+        assertEquals(feedback1, feedback2);
+    }
+}
