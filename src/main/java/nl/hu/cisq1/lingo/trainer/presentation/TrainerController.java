6bf26ba57fb379e2b16f59282dc0fff9e278efa5
aarts <aartschouten@hotmail.com>
2022-03-20 16:14:22 +0100

    -Trainercontroller basics implemented (need to add exceptions)

--- src/main/java/nl/hu/cisq1/lingo/trainer/presentation/TrainerController.java
+++ src/main/java/nl/hu/cisq1/lingo/trainer/presentation/TrainerController.java
@@ -1,4 +1,32 @@
 package nl.hu.cisq1.lingo.trainer.presentation;
 
+
+import nl.hu.cisq1.lingo.trainer.application.TrainerService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/lingo")
 public class TrainerController {
+    private TrainerService service;
+
+    public TrainerController(TrainerService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/game")
+    public GameData StartGame() {
+            GameData gameData = this.service.startGame();
+            return gameData;
+    }
+
+    @PostMapping("/game/{id}")
+    public GameData guess(@PathVariable int id, @RequestBody String attempt) {
+        return this.service.guess(id, attempt);
+    }
+
+    @PostMapping("/game/{id}")
+    public GameData startNewRound(@PathVariable int id) {
+        return this.service.startNewRound(id);
+    }
+
 }
